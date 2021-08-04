30dc56de36b7cc6ed8f762b7ec91963e40f1ab39
Lonnie <lonnieje@gmail.com>
2021-08-03 23:02:59 -0500

    Remove old config code from Events. Fixes #1

--- bonfire/Config/Events.php
+++ bonfire/Config/Events.php
@@ -10,16 +10,3 @@ use CodeIgniter\Events\Events;
 Events::on('post_controller_constructor', function() {
     service('bonfire')->boot();
 });
-
-/*
- * Post System events
- */
-Events::on('post_system', function () {
-    /**
-     * Ensure that our config values are persisted
-     * to the database.
-     */
-    if(config('Config')->persistConfig) {
-        model('ConfigModel')->persist();
-    }
-});
