fa3bd0f719d6d7e1363bcf59238ae0b2e04266ca
Lonnie <lonnieje@gmail.com>
2021-09-06 01:17:02 -0500

    Add icons to alerts to help them stand out a bit more.

--- bonfire/Views/_alerts.php
+++ bonfire/Views/_alerts.php
@@ -3,6 +3,15 @@
     foreach ($alerts as $alert):
         ?>
         <div role="alert" class="alert alert-<?= $alert['class'] ?> alert-dismissible fade show shadow">
+            <?php if($alert['class'] === 'info') : ?>
+                <i class="fas fa-info-circle text-info mx-2"></i>
+            <?php elseif($alert['class'] === 'warning') : ?>
+                <i class="fas fa-exclamation-circle text-warning mx-2"></i>
+            <?php elseif($alert['class'] === 'danger') : ?>
+                <i class="fas fa-exclamation-triangle text-danger mx-2"></i>
+            <?php elseif($alert['class'] === 'success') : ?>
+                <i class="fas fa-check-circle text-success mx-2"></i>
+            <?php endif ?>
             <?= $alert['text'] ?>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
