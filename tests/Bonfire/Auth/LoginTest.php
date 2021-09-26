cd40f4cf658f6fe05d17c33abc9268cf15a13353
Lonnie <lonnieje@gmail.com>
2021-09-25 23:21:46 -0500

    Added tests for login flows

---
+++ tests/Bonfire/Auth/LoginTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests\Bonfire\Auth;
+
+use App\Entities\User;
+use App\Models\UserModel;
+use CodeIgniter\Test\DatabaseTestTrait;
+use Sparks\Shield\Authentication\Actions\Email2FA;
+use Tests\Support\TestCase;
+
+class LoginTest extends TestCase
+{
+    use DatabaseTestTrait;
+
+    protected $refresh = true;
+    protected $namespace = null;
+
+    public function testCanViewLoginPage()
+    {
+        $response = $this->get(route_to('login'));
+
+        $response->assertOK();
+        $response->assertSee('Login');
+    }
+
+    public function testLoginSuccess()
+    {
+        $user = fake(UserModel::class);
+        $user->createEmailIdentity(['email' => 'foo@example.com', 'password' => 'alsdkfja;sldkfj']);
+
+        // Submit registration form
+        $response = $this->post(route_to('login'), [
+            'email' => 'foo@example.com',
+            'password' => 'alsdkfja;sldkfj',
+        ]);
+
+        $response->assertRedirectTo(site_url('admin'));
+
+        $response->assertSessionHas('logged_in');
+    }
+
+    public function testAllowRememberShowsUp()
+    {
+        setting('Auth.sessionConfig', [
+            'field'              => 'logged_in',
+            'allowRemembering'   => true,
+            'rememberCookieName' => 'remember',
+            'rememberLength'     => 30 * DAY,
+        ]);
+
+        $response = $this->get(route_to('login'));
+
+        $response->assertSee(lang('Auth.rememberMe'));
+
+        setting('Auth.sessionConfig', [
+            'field'              => 'logged_in',
+            'allowRemembering'   => false,
+            'rememberCookieName' => 'remember',
+            'rememberLength'     => 30 * DAY,
+        ]);
+
+        $response = $this->get(route_to('login'));
+
+        $response->assertDontSee(lang('Auth.rememberMe'));
+    }
+
+    public function testLoginTo2FAAction()
+    {
+        setting('Auth.actions', [
+            'login' => Email2FA::class,
+            'register' => null
+        ]);
+
+        $user = fake(UserModel::class);
+        $user->createEmailIdentity(['email' => 'foo@example.com', 'password' => 'alsdkfja;sldkfj']);
+
+        // Submit registration form
+        $response = $this->post(route_to('login'), [
+            'email' => 'foo@example.com',
+            'password' => 'alsdkfja;sldkfj',
+        ]);
+
+        $response->assertRedirectTo('/auth/a/show');
+    }
+}
