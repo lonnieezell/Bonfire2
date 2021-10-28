849989db419a9b4218035c60d266f408c6d3e3b7
Lonnie Ezell <lonnieje@gmail.com>
2021-10-27 22:44:43 -0500

    Fix routes

--- bonfire/Modules/Tools/Config/Routes.php
+++ bonfire/Modules/Tools/Config/Routes.php
@@ -2,8 +2,8 @@
 
 namespace Bonfire\Modules\Tools\Config;
 
-$routes->get(ADMIN_AREA.'tools/system-info', '\Bonfire\Tools\Controllers\SystemInfoController::index', ['as' => 'sys-info']);
-$routes->get(ADMIN_AREA.'tools/php-info', '\Bonfire\Tools\Controllers\SystemInfoController::phpInfo');
-$routes->match(['get', 'post'], ADMIN_AREA.'tools/logs', '\Bonfire\Tools\Controllers\LogsController::index', ['as' => 'sys-logs']);
-$routes->get(ADMIN_AREA.'tools/view-log/(:segment)', '\Bonfire\Tools\Controllers\LogsController::view/$1', ['as' => 'view-log']);
-$routes->post(ADMIN_AREA.'tools/delete-log', '\Bonfire\Tools\Controllers\LogsController::delete', ['as' => 'log-delete']);
+$routes->get(ADMIN_AREA.'/tools/system-info', '\Bonfire\Tools\Controllers\SystemInfoController::index', ['as' => 'sys-info']);
+$routes->get(ADMIN_AREA.'/tools/php-info', '\Bonfire\Tools\Controllers\SystemInfoController::phpInfo');
+$routes->match(['get', 'post'], ADMIN_AREA.'/tools/logs', '\Bonfire\Tools\Controllers\LogsController::index', ['as' => 'sys-logs']);
+$routes->get(ADMIN_AREA.'/tools/view-log/(:segment)', '\Bonfire\Tools\Controllers\LogsController::view/$1', ['as' => 'view-log']);
+$routes->post(ADMIN_AREA.'/tools/delete-log', '\Bonfire\Tools\Controllers\LogsController::delete', ['as' => 'log-delete']);
