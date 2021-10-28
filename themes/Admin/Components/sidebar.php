61a70d10bb526a1996b510edc24818a048ffbba3
Lonnie Ezell <lonnieje@gmail.com>
2021-10-27 22:55:06 -0500

    Remove sidebar collapsing for now

--- themes/Admin/Components/sidebar.php
+++ themes/Admin/Components/sidebar.php
@@ -13,22 +13,18 @@
     <?php if(isset($menu)) : ?>
         <?php foreach($menu->collections() as $collection) : ?>
 
-        <div x-data="{expanded: <?= ! $collection->isCollapsible() ? 'true' : 'false' ?>}">
+        <div>
             <?php if($collection->isCollapsible()) : ?>
-                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 collapsible"
-                    @click="expanded = ! expanded">
+                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1">
                     <span><?= $collection->title ?></span>
-                    <span>&plus;</span>
                 </h6>
-
-                <div class="px-3">
             <?php else : ?>
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1">
                     <span><?= $collection->title ?></span>
                 </h6>
             <?php endif ?>
 
-            <ul class="nav flex-column mb-2" x-show="expanded">
+            <ul class="nav flex-column mb-2 px-0">
                 <?php foreach($collection->items() as $item) : ?>
                     <li class="nav-item">
                         <a class="nav-link <?= url_is($item->url.'*') ? 'active' : '' ?>" href="<?= $item->url ?>">
@@ -38,10 +34,6 @@
                     </li>
                 <?php endforeach ?>
             </ul>
-
-            <?php if($collection->isCollapsible()) : ?>
-                </div>
-            <?php endif ?>
         </div>
         <?php endforeach ?>
     <?php endif ?>
