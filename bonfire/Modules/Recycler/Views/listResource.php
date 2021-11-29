547a97ad51e4073b6600832fe4406fdfb73c5a65
Antonio Sanna <atsanna@users.noreply.github.com>
2021-11-29 09:52:38 +0100

    Manage dropdown using hmtx

--- bonfire/Modules/Recycler/Views/listResource.php
+++ bonfire/Modules/Recycler/Views/listResource.php
@@ -7,15 +7,15 @@
         <div class="col">
             <h2>Recycler</h2>
         </div>
-		<?php if(count($resources) > 1) : ?>
+        <?php if(count($resources) > 1) : ?>
             <div class="col-auto">
                 <select name="r" class="form-select" hx-get="?" hx-target="#resource" hx-boost="true">
-					<?php foreach($resources as $alias => $details) : ?>
-                        <option value="<?= strtolower($alias) ?>" <?= ( isset($_GET) && isset($_GET['r']) && $_GET['r']===strtolower($alias)) ?  'selected' : ''?>><?= $details['label'] ?></option>
-					<?php endforeach ?>
+                <?php foreach($resources as $alias => $details) : ?>
+                    <option value="<?= strtolower($alias) ?>" <?= ( isset($_GET) && isset($_GET['r']) && $_GET['r']===strtolower($alias)) ?  'selected' : ''?>><?= $details['label'] ?></option>
+                <?php endforeach ?>
                 </select>
             </div>
-		<?php endif ?>
+        <?php endif ?>
     </div>
 </x-page-head>
 
@@ -24,48 +24,48 @@
     <fieldset id="resource">
         <legend><?= $currentResource['label'] ?></legend>
 
-		<?php if (isset($items) && count($items)) : ?>
+        <?php if (isset($items) && count($items)) : ?>
             <p><?= $pager->getTotal() ?> result(s)</p>
 
             <table class="table table-striped table-hover">
                 <thead>
                 <tr>
-					<?php foreach($currentResource['columns'] as $column) : ?>
-                        <th><?= esc(ucwords(str_replace('_', ' ', $column))) ?></th>
-					<?php endforeach ?>
+                <?php foreach($currentResource['columns'] as $column) : ?>
+                    <th><?= esc(ucwords(str_replace('_', ' ', $column))) ?></th>
+                <?php endforeach ?>
                     <th></th>
                 </tr>
                 </thead>
                 <tbody>
-				<?php foreach ($items as $item) : ?>
-                    <tr>
-						<?php foreach($currentResource['columns'] as $column) : ?>
+                    <?php foreach ($items as $item) : ?>
+                        <tr>
+                        <?php foreach($currentResource['columns'] as $column) : ?>
                             <td><?= esc($item[$column] ?? '') ?></td>
-						<?php endforeach ?>
-                        <td class="text-end">
-                            <a href="/<?= ADMIN_AREA ?>/recycler/restore/<?= $currentAlias .'/'. $item['id'] ?>"
-                               class="text-success" title="Restore"
-                               onclick="return confirm('Restore this record?');"
-                            >
-                                <i class="fas fa-trash-restore"></i>
-                            </a>
-                            &nbsp;
-                            <a href="/<?= ADMIN_AREA ?>/recycler/purge/<?= $currentAlias .'/'. $item['id'] ?>"
-                               class="text-danger" title="Purge forever"
-                               onclick="return confirm('Permanently delete this record?');"
-                            >
-                                <i class="fas fa-minus-circle"></i>
-                            </a>
-                        </td>
-                    </tr>
-				<?php endforeach ?>
+                        <?php endforeach ?>
+                            <td class="text-end">
+                                <a href="/<?= ADMIN_AREA ?>/recycler/restore/<?= $currentAlias .'/'. $item['id'] ?>"
+                                   class="text-success" title="Restore"
+                                   onclick="return confirm('Restore this record?');"
+                                >
+                                    <i class="fas fa-trash-restore"></i>
+                                </a>
+                                &nbsp;
+                                <a href="/<?= ADMIN_AREA ?>/recycler/purge/<?= $currentAlias .'/'. $item['id'] ?>"
+                                   class="text-danger" title="Purge forever"
+                                   onclick="return confirm('Permanently delete this record?');"
+                                >
+                                    <i class="fas fa-minus-circle"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
                 </tbody>
             </table>
-		<?php else : ?>
+        <?php else : ?>
             <div class="alert alert-info">
                 No <?= $currentAlias ?> found.
             </div>
-		<?php endif ?>
+        <?php endif ?>
 
     </fieldset>
 
