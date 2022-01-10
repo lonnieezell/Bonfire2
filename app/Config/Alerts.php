865fbe80fe71a0de4d2c1e75d507341e701536a8
MGatner <mgatner@icloud.com>
2022-01-10 16:03:01 +0000

    Upgrade to Alerts 3.0

--- app/Config/Alerts.php
+++ app/Config/Alerts.php
@@ -6,12 +6,10 @@ use Tatter\Alerts\Config\Alerts as TatterAlerts;
 
 class Alerts extends TatterAlerts
 {
-    // prefix for SESSION variables and HTML classes, to prevent collision
-    public $prefix = 'alerts-';
-
-    // Template to use for HTML output
+    /**
+     * Template to use for HTML output.
+     *
+     * @var string
+     */
     public $template = 'Bonfire\\Views\\_alerts';
-
-    // Whether to check session flashdata for common alert keys
-    public $getflash = true;
 }
