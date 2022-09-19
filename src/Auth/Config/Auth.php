e707e92b86e5fcbff54b9a6a4dff9fb77c234680
Radoslav Georgiev <kpeu3u@gmail.com>
2022-09-19 16:56:09 +0300

    Update Auth Config file

--- src/Auth/Config/Auth.php
+++ src/Auth/Config/Auth.php
@@ -2,8 +2,12 @@
 
 namespace Bonfire\Auth\Config;
 
+use Bonfire\Users\Models\UserModel;
+use CodeIgniter\Shield\Authentication\Actions\ActionInterface;
+use CodeIgniter\Shield\Authentication\AuthenticatorInterface;
 use CodeIgniter\Shield\Authentication\Authenticators\AccessTokens;
 use CodeIgniter\Shield\Authentication\Authenticators\Session;
+use CodeIgniter\Shield\Authentication\Passwords\ValidatorInterface;
 use CodeIgniter\Shield\Config\Auth as ShieldAuth;
 
 class Auth extends ShieldAuth
@@ -15,7 +19,7 @@ class Auth extends ShieldAuth
      */
     public array $views = [
         'layout'                      => 'master',
-        'email_layout'                => '\Bonfire\Views\email.php',
+        'email_layout'                => '\Bonfire\Views\email',
         'login'                       => '\Bonfire\Views\Auth\login',
         'register'                    => '\Bonfire\Views\Auth\register',
         'forgotPassword'              => '\CodeIgniter\Shield\Views\forgot_password',
@@ -35,9 +39,8 @@ class Auth extends ShieldAuth
      * Redirect urLs
      * --------------------------------------------------------------------
      * The default URL that a user will be redirected to after
-     * various auth actions. If you need more flexibility you
-     * should extend the appropriate controller and overrider the
-     * `getRedirect()` methods to apply any logic you may need.
+     * various auth actions. If you need more flexibility you can
+     * override the `getUrl()` method to apply any logic you may need.
      */
     public array $redirects = [
         'register' => '/',
@@ -52,13 +55,17 @@ class Auth extends ShieldAuth
      * Specifies the class that represents an action to take after
      * the user logs in or registers a new account at the site.
      *
+     * You must register actions in the order of the actions to be performed.
+     *
      * Available actions with Shield:
-     * - login:    Shield\Authentication\Actions\Email2FA
-     * - register: Shield\Authentication\Actions\EmailActivate
+     * - register: 'CodeIgniter\Shield\Authentication\Actions\EmailActivator'
+     * - login:    'CodeIgniter\Shield\Authentication\Actions\Email2FA'
+     *
+     * @var array<string, class-string<ActionInterface>|null>
      */
     public array $actions = [
-        'login'    => null,
         'register' => null,
+        'login'    => null,
     ];
 
     /**
@@ -68,7 +75,9 @@ class Auth extends ShieldAuth
      * The available authentication systems, listed
      * with alias and class name. These can be referenced
      * by alias in the auth helper:
-     *      auth('api')->attempt($credentials);
+     *      auth('tokens')->attempt($credentials);
+     *
+     * @var array<string, class-string<AuthenticatorInterface>>
      */
     public array $authenticators = [
         'tokens'  => AccessTokens::class,
@@ -100,7 +109,7 @@ class Auth extends ShieldAuth
      * --------------------------------------------------------------------
      * Default Authenticator
      * --------------------------------------------------------------------
-     * The authentication handler to use when none is specified.
+     * The Authenticator to use when none is specified.
      * Uses the $key from the $authenticators array above.
      */
     public string $defaultAuthenticator = 'session';
@@ -109,14 +118,25 @@ class Auth extends ShieldAuth
      * --------------------------------------------------------------------
      * Authentication Chain
      * --------------------------------------------------------------------
-     * The authentication handlers to test logged in status against
-     * when using the 'chain' filter. Each handler listed will be checked.
+     * The Authenticators to test logged in status against
+     * when using the 'chain' filter. Each Authenticator listed will be checked.
      * If no match is found, then the next in the chain will be checked.
+     *
+     * @var string[]
+     * @phpstan-var list<string>
      */
     public array $authenticationChain = [
         'session',
         'tokens',
     ];
+
+    /**
+     * --------------------------------------------------------------------
+     * Allow Registration
+     * --------------------------------------------------------------------
+     * Determines whether users can register for the site.
+     */
+    public bool $allowRegistration = true;
 
     /**
      * --------------------------------------------------------------------
@@ -126,14 +146,6 @@ class Auth extends ShieldAuth
      * logged in user on every page request.
      */
     public bool $recordActiveDate = true;
-
-    /**
-     * --------------------------------------------------------------------
-     * Allow Registration
-     * --------------------------------------------------------------------
-     * Determines whether users can register for the site.
-     */
-    public bool $allowRegistration = true;
 
     /**
      * --------------------------------------------------------------------
@@ -152,20 +164,23 @@ class Auth extends ShieldAuth
      * Magic Link Lifetime
      * --------------------------------------------------------------------
      * Specifies the amount of time, in seconds, that a magic link is valid.
+     * You can use Time Constants or any desired number.
      */
-    public int $magicLinkLifetime = 1 * HOUR;
+    public int $magicLinkLifetime = HOUR;
 
     /**
      * --------------------------------------------------------------------
-     * Session Handler Configuration
+     * Session Authenticator Configuration
      * --------------------------------------------------------------------
-     * These settings only apply if you are using the Session Handler
+     * These settings only apply if you are using the Session Authenticator
      * for authentication.
      *
-     * - field                  The name of the key the logged in user is stored in session
+     * - field                  The name of the key the current user info is stored in session
      * - allowRemembering       Does the system allow use of "remember-me"
      * - rememberCookieName     The name of the cookie to use for "remember-me"
      * - rememberLength         The length of time, in seconds, to remember a user.
+     *
+     * @var array<string, bool|int|string>
      */
     public array $sessionConfig = [
         'field'              => 'logged_in',
@@ -190,16 +205,15 @@ class Auth extends ShieldAuth
      * The PasswordValidator class runs the password through all of these
      * classes, each getting the opportunity to pass/fail the password.
      * You can add custom classes as long as they adhere to the
-     * Password\ValidatorInterface.
+     * CodeIgniter\Shield\Authentication\Passwords\ValidatorInterface.
      *
-     * @var class-string[]
-     * @phpstan-ignore-next-line
+     * @var class-string<ValidatorInterface>[]
      */
     public array $passwordValidators = [
         'CodeIgniter\Shield\Authentication\Passwords\CompositionValidator',
         'CodeIgniter\Shield\Authentication\Passwords\NothingPersonalValidator',
         'CodeIgniter\Shield\Authentication\Passwords\DictionaryValidator',
-        //'CodeIgniter\Shield\Authentication\Passwords\PwnedValidator',
+        // 'CodeIgniter\Shield\Authentication\Passwords\PwnedValidator',
     ];
 
     /**
@@ -315,7 +329,6 @@ class Auth extends ShieldAuth
      * OTHER SETTINGS
      * ////////////////////////////////////////////////////////////////////
      */
-
     /**
      * --------------------------------------------------------------------
      * User Provider
@@ -327,7 +340,6 @@ class Auth extends ShieldAuth
      * CodeIgniter\Shield\Models\UserModel.
      *
      * @var class-string<UserModel>
-     * @phpstan-ignore-next-line
      */
     public string $userProvider = 'Bonfire\Users\Models\UserModel';
 
