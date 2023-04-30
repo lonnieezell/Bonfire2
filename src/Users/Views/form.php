20f00d330f13c3e738719bc832cc30c787a07adb
Donatas Glodenis <dgvirtual@akl.lt>
2023-04-30 07:10:20 +0300

    Add missing Codeigniter log levels, remove ALL (#309)
    * Add all Codeigniter log levels, remove ALL
    (ALL is never used in writing logs)
    
    * User with me.* permsissions can change one's data
    (excluding permissions and groups data)

--- src/Users/Views/form.php
+++ src/Users/Views/form.php
@@ -1,27 +1,27 @@
 <?php $this->extend('master') ?>
 
 <?php $this->section('main') ?>
-    <x-page-head>
-        <a href="/<?= ADMIN_AREA ?>/users" class="back">&larr; Users</a>
-        <h2><?= isset($user) ? 'Edit User' : 'New User' ?></h2>
-    </x-page-head>
+<x-page-head>
+    <a href="/<?= ADMIN_AREA ?>/users" class="back">&larr; Users</a>
+    <h2><?= isset($user) ? 'Edit User' : 'New User' ?></h2>
+</x-page-head>
 
-    <?php if (isset($user) && $user->deleted_at !== null) : ?>
-        <div class="alert danger">
-            This user was deleted on <?= $user->deleted_at->humanize() ?>.
-            <a href="#">Restore user?</a>
-        </div>
-    <?php endif ?>
+<?php if (isset($user) && $user->deleted_at !== null) : ?>
+    <div class="alert danger">
+        This user was deleted on <?= $user->deleted_at->humanize() ?>.
+        <a href="#">Restore user?</a>
+    </div>
+<?php endif ?>
 
-    <?= view('Bonfire\Users\Views\_tabs', ['tab' => 'details', 'user' => $user ?? null]) ?>
+<?= view('Bonfire\Users\Views\_tabs', ['tab' => 'details', 'user' => $user ?? null]) ?>
 
-    <x-admin-box>
+<x-admin-box>
 
     <?php if (isset($user) && $user !== null) : ?>
         <form action="<?= $user->adminLink('/save') ?>" method="post" enctype="multipart/form-data">
-    <?php else : ?>
-        <form action="<?= (new \Bonfire\Users\User())->adminLink('/save') ?>" method="post" enctype="multipart/form-data">
-    <?php endif ?>
+        <?php else : ?>
+            <form action="<?= (new \Bonfire\Users\User())->adminLink('/save') ?>" method="post" enctype="multipart/form-data">
+            <?php endif ?>
             <?= csrf_field() ?>
 
             <?php if (isset($user) && $user !== null) : ?>
@@ -50,8 +50,7 @@
                                 <label for="email" class="form-label">Email Address</label>
                                 <div class="input-group">
                                     <span class="input-group-text" id="email">@</span>
-                                    <input type="text" name="email" class="form-control" autocomplete="email"
-                                           value="<?= old('email', $user->email ?? '') ?>">
+                                    <input type="text" name="email" class="form-control" autocomplete="email" value="<?= old('email', $user->email ?? '') ?>">
                                 </div>
                                 <?php if (has_error('email')) : ?>
                                     <p class="text-danger"><?= error('email') ?></p>
@@ -60,8 +59,7 @@
                             <!-- Username -->
                             <div class="form-group col-12 col-sm-6">
                                 <label for="username" class="form-label">Username</label>
-                                <input type="text" name="username" class="form-control" autocomplete="username"
-                                       value="<?= old('username', $user->username ?? '') ?>">
+                                <input type="text" name="username" class="form-control" autocomplete="username" value="<?= old('username', $user->username ?? '') ?>">
                                 <?php if (has_error('username')) : ?>
                                     <p class="text-danger"><?= error('username') ?></p>
                                 <?php endif ?>
@@ -72,8 +70,7 @@
                             <!-- First Name -->
                             <div class="form-group col-12 col-sm-6">
                                 <label for="first_name" class="form-label">First Name</label>
-                                <input type="text" name="first_name" class="form-control" autocomplete="first_name"
-                                       value="<?= old('first_name', $user->first_name ?? '') ?>">
+                                <input type="text" name="first_name" class="form-control" autocomplete="first_name" value="<?= old('first_name', $user->first_name ?? '') ?>">
                                 <?php if (has_error('first_name')) : ?>
                                     <p class="text-danger"><?= error('first_name') ?></p>
                                 <?php endif ?>
@@ -81,8 +78,7 @@
                             <!-- Last Name -->
                             <div class="form-group col-12 col-sm-6">
                                 <label for="last_name" class="form-label">Last Name</label>
-                                <input type="text" name="last_name" class="form-control" autocomplete="last_name"
-                                       value="<?= old('last_name', $user->last_name ?? '') ?>">
+                                <input type="text" name="last_name" class="form-control" autocomplete="last_name" value="<?= old('last_name', $user->last_name ?? '') ?>">
                                 <?php if (has_error('last_name')) : ?>
                                     <p class="text-danger"><?= error('last_name') ?></p>
                                 <?php endif ?>
@@ -92,24 +88,27 @@
                 </div>
             </fieldset>
 
-            <fieldset>
-                <legend>Groups</legend>
+                <fieldset>
+                    <legend>Groups</legend>
 
-                <p>Select one or more groups for the user to belong to.</p>
+                    <?php if (auth()->user()->can('users.edit')) : ?>
+                        <p>Select one or more groups for the user to belong to.</p>
+                    <?php else : ?>
+                        <p>Groups that the user belongs to (you do not have permission to modify the list).</p>
+                    <?php endif; ?>
 
-                <div class="row">
-                    <div class="form-group col-12 col-sm-6">
-                        <select name="groups[]" multiple="multiple" class="form-control">
-                        <?php foreach ($groups as $group => $info) : ?>
-                            <option value="<?= $group ?>" <?php if (isset($user) && $user->inGroup($group)) : ?> selected <?php endif ?>>
-                                <?= $info['title'] ?? $group ?>
-                            </option>
-                        <?php endforeach ?>
-                        </select>
+                    <div class="row">
+                        <div class="form-group col-12 col-sm-6">
+                            <select name="groups[]" multiple="multiple" class="form-control" <?php if (! auth()->user()->can('users.edit')) echo ' disabled' ?>>
+                                <?php foreach ($groups as $group => $info) : ?>
+                                    <option value="<?= $group ?>" <?php if (isset($user) && $user->inGroup($group)) : ?> selected <?php endif ?>>
+                                        <?= $info['title'] ?? $group ?>
+                                    </option>
+                                <?php endforeach ?>
+                            </select>
+                        </div>
                     </div>
-                </div>
-
-            </fieldset>
+                </fieldset>
 
             <!-- User Meta Fields -->
             <div class="row">
@@ -120,8 +119,8 @@
                 <input type="submit" value="Save User" class="btn btn-primary btn-lg">
             </div>
 
-        </form>
+            </form>
 
-    </x-admin-box>
+</x-admin-box>
 
 <?php $this->endSection() ?>
