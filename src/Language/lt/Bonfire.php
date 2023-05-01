6e645558544a2743783fb0559e63e0281c135450
Donatas Glodenis <dgvirtual@akl.lt>
2023-05-01 10:26:34 +0300

    Add Lithuanian localization

---
+++ src/Language/lt/Bonfire.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of Bonfire.
+ *
+ * (c) Lonnie Ezell <lonnieje@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+return [
+    'resourceNotFound'     => 'Nepavyko rasti {0}.',
+    'resourceSaved'        => '{0} buvo įrašytas.',
+    'resourcesSaved'       => '{0} buvo įrašyti.',
+    'resourceDeleted'      => '{0} buvo ištrintas.',
+    'resourcesDeleted'     => '{0} buvo ištrinti.',
+    'resourceRestored'     => '{0} buvo atkurtas.',
+    'unknownSaveError'     => 'Nežinoma klaida įrašant resursą ({0}).',
+    'unknownError'         => 'Nežinoma klaida.',
+    'resourcesNotSelected' => 'Nebuvo pažymėta {0}.',
+
+    'deleteResource' => 'Trinti šį {0}?',
+    'delete'         => 'Trinti',
+    'edit'           => 'Redaguoti',
+
+    'notAuthorized' => 'Jūs neturite tinkamų leidimų.',
+    'unknownAction' => 'Mums nepavyko atpažinti prašomo veiksmo.',
+    'magicLinkInfo' => 'Mes išsiųsime el. laišką su nuoroda, kurią paspaudę galėsite prisijungti.',
+
+    'lastModified' => 'Vėliausiai keista',
+    'fileSize'     => 'Failo dydis',
+];
