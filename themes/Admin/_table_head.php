d083e3977e76b254bcecf47c5a4d6f4ff36d48bb
Donatas Glodenis <dgvirtual@akl.lt>
2023-05-02 19:37:51 +0300

    Fix capitalization for non-ASCII characters

--- themes/Admin/_table_head.php
+++ themes/Admin/_table_head.php
@@ -7,7 +7,7 @@
             </th>
         <?php endif ?>
     <?php foreach ($headers as $column => $title) : ?>
-        <th><?= strtoupper($title) ?></th>
+        <th><?= mb_strtoupper($title) ?></th>
     <?php endforeach ?>
     <?php if(auth()->user()->can('users.edit') || auth()->user()->can('users.delete')): ?>
         <th></th>
