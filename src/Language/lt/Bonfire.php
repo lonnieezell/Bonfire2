f393350bbe37391557f13ad2ee76c1d42dbd7c8c
Donatas Glodenis <dgvirtual@akl.lt>
2023-05-03 20:10:24 +0300

    Basic localization - adding lt translation

---
+++ src/Language/lt/Bonfire.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of Bonfire.
+ *
+ * (c) Lonnie Ezell <lonnieje@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+return [
+    'resourceNotFound'     => 'Nepavyko rasti {0}.',
+    'resourceSaved'        => '{0} buvo įrašytas.',
+    'resourcesSaved'       => '{0} buvo įrašyti.',
+    'resourceDeleted'      => '{0} buvo ištrintas.',
+    'resourcesDeleted'     => '{0} buvo ištrinti.',
+    'resourceRestored'     => '{0} buvo atkurtas.',
+    'unknownSaveError'     => 'Nežinoma klaida įrašant resursą ({0}).',
+    'unknownError'         => 'Nežinoma klaida.',
+    'resourcesNotSelected' => 'Nebuvo pažymėta {0}.',
+
+    'deleteResource' => 'Trinti šį {0}?',
+    'delete'         => 'Trinti',
+    'edit'           => 'Redaguoti',
+
+    'notAuthorized' => 'Jūs neturite tinkamų leidimų.',
+    'unknownAction' => 'Mums nepavyko atpažinti prašomo veiksmo.',
+    'magicLinkInfo' => 'Mes išsiųsime el. laišką su nuoroda, kurią paspaudę galėsite prisijungti.',
+
+    'lastModified' => 'Vėliausiai keista',
+    'fileSize'     => 'Failo dydis',
+
+    //collections: 
+    'contentModTitle'   => 'Turinys',
+    'settingsModTitle'  => 'Nustatymai',
+    'toolsModTitle'     => 'Įrankiai',
+    'generalModTitle'   => 'Bendrieji',
+
+    //theme:
+    'filter'            => 'Filtras',
+    'dashboard'         => 'Skydelis',
+    'searchResults'     => 'Paieškos rezultatai',
+    'nothingFound'      => 'Nerasta paieškos kriterijus atitinkančių įrašų. Pakeiskite paiešką ir bandykite iš naujo.',
+    'seeAll'            => 'Žiūrėti visus',
+    'search'            => 'Ieškoti',
+    'myAccount'         => 'Mano paskyra',
+    'signOut'           => 'Išsiregistruoti',
+    'offlineNotice'     => 'Svetainė išjungta. Įjunkite ją',
+    'here'              => 'čia',
+    'collapseSidebar'   => 'Susiaurinti',
+
+    // Dashboard:
+    'quickLinks'    =>  'Patogios nuorodos',
+];
