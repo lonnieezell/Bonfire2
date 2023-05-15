949f5563d3eae24dbfe78afc0e6a91b11f65f6c7
Donatas Glodenis <dgvirtual@akl.lt>
2023-05-15 06:24:32 +0300

    Fix for opening create new user page (#339)
    when setting('Users.useGravatar') is true; possible final fix for #267

--- src/Users/User.php
+++ src/Users/User.php
@@ -54,7 +54,7 @@ class User extends ShieldUser
      */
     public function avatarLink(?int $size = null): string
     {
-        if (empty($this->avatar)) {
+        if (isset($this->id) && empty($this->avatar)) {
             // Default from Gravatar
             if (setting('Users.useGravatar')) {
                 $hash = md5(strtolower(trim($this->email)));
