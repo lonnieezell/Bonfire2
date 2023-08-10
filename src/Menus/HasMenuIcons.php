f1e99f66d628d3c5ed2eef95a7d7a47569843bc4
Krishna Gujjjar <42290039+krishna-gujjjar@users.noreply.github.com>
2023-08-10 19:31:54 +0530

    VSCode  Suggestions & DocBlock Comments Fixed (#380)
    * 🐞 fix: Fix vscode suggestion not showing (#375)
    
    * chore(Menus): Better Comments & PHP DocBlock
    
    `php-cs` based PHP DocBlock & better comments on Menu Component.
    
    * ✨style(Menus): Remove unnecessary comment

--- src/Menus/HasMenuIcons.php
+++ src/Menus/HasMenuIcons.php
@@ -7,37 +7,48 @@
  *
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
+ *
+ * php version 8.0
+ *
+ * @category Menus
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @see      https://github.com/lonnieezell/Bonfire2/
  */
 
 namespace Bonfire\Menus;
 
+/**
+ * Trait HasMenuIcons
+ */
 trait HasMenuIcons
 {
     /**
      * FontAwesome 5 icon name
-     *
-     * @var string|null
      */
-    protected $faIcon;
+    protected ?string $faIcon;
 
     /**
-     * URL to icon, if an image.
-     *
-     * @var string|null
+     * URL of the icon, if it's an image.
      */
-    protected $iconUrl;
+    protected ?string $iconUrl;
 
     /**
-     * Sets the FontAwesome icon name, like:
+     * Sets the FontAwesome icon name
      *
-     * - fa-pencil
-     * - fal fa-alarm-clock
+     * Here is an inline example:
      *
-     * @return $this
+     * ```js
+     * setFontAwesomeIcon('fa-pencil');
+     * setFontAwesomeIcon('fal fa-alarm-clock');
+     * ```
+     *
+     * @param string $iconName FontAwesome Icon Class name
+     *
+     * @return self
      */
-    public function setFontAwesomeIcon(string $icon)
+    public function setFontAwesomeIcon(string $iconName)
     {
-        $this->faIcon = $icon;
+        $this->faIcon = $iconName;
 
         return $this;
     }
@@ -45,7 +56,9 @@ trait HasMenuIcons
     /**
      * Sets the URL to the icon, if it's an image.
      *
-     * @return $this
+     * @param string $url Custom Icon URL
+     *
+     * @return self
      */
     public function setIconUrl(string $url)
     {
@@ -55,43 +68,49 @@ trait HasMenuIcons
     }
 
     /**
-     * Returns the full icon tag: either a <i> tag for FontAwesome
-     * icons, or an <img> tag for images.
+     * Returns HTML tag depends Menu Icon type `iconUrl` or `faIcon`
+     *
+     * Returns the full icon tag: either a `<i>` tag for FontAwesome icons,
+     * or an `<img>` tag for images.
+     *
+     * @param string $classOrIconUrl FontAwesome Icon Class Name or Icon URL
      */
-    public function icon(string $class = ''): string
+    public function icon(string $classOrIconUrl = ''): string
     {
         if (! empty($this->faIcon)) {
-            return $this->buildFontAwesomeIconTag($class);
+            return $this->buildFontAwesomeIconTag($classOrIconUrl);
         }
         if (! empty($this->iconUrl)) {
-            return $this->buildImageIconTag($class);
+            return $this->buildImageIconTag($classOrIconUrl);
         }
 
         return '';
     }
 
     /**
-     * Returns the full FontAwesome tag.
+     * Returns FontAwesome HTML Icon Tag
+     *
+     * Eg: `<i class="fas fa-users"></i>`
+     *
+     * @param string $className Custom class name for FontAwesome Icon
      */
-    protected function buildFontAwesomeIconTag(string $class): string
+    protected function buildFontAwesomeIconTag(string $className): string
     {
-        $class = ! empty($class)
-            ? " {$class}"
-            : '';
+        $class = ! empty($className) ? " {$className}" : '';
 
         return "<i class=\"{$this->faIcon}{$class}\"></i>";
     }
 
     /**
-     * Returns a full img tag for our icon.
+     * Returns Image tag for custom icon
      *
-     * @return string
+     * Eg: `<img src="https://example.com" class="some-class"/>`
+     *
+     * @param string $class Custom Icon class name
      */
-    protected function buildImageIconTag(string $class)
+    protected function buildImageIconTag(string $class): string
     {
-        $class = ! empty($class)
-            ? "class=\"{$class}\" "
-            : '';
+        $class = ! empty($class) ? "class=\"{$class}\" " : '';
 
         $iconUrl = strpos($this->iconUrl, '://') !== false
             ? $this->iconUrl
