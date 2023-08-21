8021df56f61afd3d125738bced62ad74ac65554d
Donatas Glodenis <dgvirtual@akl.lt>
2023-08-21 06:23:33 +0300

    Turn "Last Active" checkbox filter to radio filter (#388)
    * Turn "active within" filter to radio filter
    add aditional option "never"
    
    * Updated documentation to describe new filter key

--- src/Views/_filter_list.php
+++ src/Views/_filter_list.php
@@ -7,19 +7,29 @@
     >
         <?= csrf_field() ?>
 
-        <?php foreach ($filters as $key => $filter): ?>
+        <?php foreach ($filters as $key => $filter) : ?>
             <h2><?= $filter['title'] ?></h2>
 
             <ul class="list-unstyled">
-            <?php foreach ($filter['options'] as $value => $name): ?>
-                <li class="form-check">
-                    <input class="form-check-input filter-check" type="checkbox" name="filters[<?= $key ?>][<?= $value ?>]"
-                           value="<?= $value ?>" id="<?= $key . ':' . $value ?>>">
-                    <label class="form-check-label" for="<?= $key . ':' . $value ?>">
-                        <?= $name ?>
-                    </label>
-                </li>
-            <?php endforeach ?>
+            <?php if (isset($filter['type']) && $filter['type'] === 'radio'): ?>
+                <?php foreach ($filter['options'] as $value => $name): ?>
+                    <li class="form-check">
+                        <input class="form-check-input filter-check" type="radio" name="filters[<?= $key ?>]" value="<?= $value ?>" id="<?= $key . ':' . $value ?>>" <?= $key === 'all' ?: ' checked' ?>>
+                        <label class="form-check-label" for="<?= $key . ':' . $value ?>">
+                            <?= $name ?>
+                        </label>
+                    </li>
+                <?php endforeach ?>
+            <?php else :  ?>
+                <?php foreach ($filter['options'] as $value => $name) : ?>
+                    <li class="form-check">
+                        <input class="form-check-input filter-check" type="checkbox" name="filters[<?= $key ?>][<?= $value ?>]" value="<?= $value ?>" id="<?= $key . ':' . $value ?>>">
+                        <label class="form-check-label" for="<?= $key . ':' . $value ?>">
+                            <?= $name ?>
+                        </label>
+                    </li>
+                <?php endforeach ?>
+            <?php endif; ?>
             </ul>
         <?php endforeach ?>
     </form>
