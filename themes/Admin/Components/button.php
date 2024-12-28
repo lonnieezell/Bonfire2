551a9219d4c87b725f26cf2c99ba80fb835ea4f8
Donatas Glodenis <dg@lapas.info>
2024-12-28 17:15:30 +0200

    Add onclick functionality to x-button

--- themes/Admin/Components/button.php
+++ themes/Admin/Components/button.php
@@ -1,3 +1,3 @@
-<button type="<?= $type ?? 'submit' ?>" class="btn btn-<?= $color ?? 'primary' ?> btn-lg">
+<button <?= $onclick ? 'onclick="' . $onclick . '"' : '' ?> type="<?= $type ?? 'submit' ?>" class="btn btn-<?= $color ?? 'primary' ?> btn-lg">
     <?= $slot ?? '' ?>
 </button>
