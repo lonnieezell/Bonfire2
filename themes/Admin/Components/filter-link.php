d70dca33e4466fdbe39837dbdcfb4da5b0318b81
Donatas Glodenis <dg@lapas.info>
2024-12-30 20:02:58 +0200

    Style fixes

--- themes/Admin/Components/filter-link.php
+++ themes/Admin/Components/filter-link.php
@@ -1,3 +1,3 @@
 <div class="d-flex justify-content-end">
-    <a href="#" x-on:click="filtered = ! filtered"><?= lang('Bonfire.filter' )?></a>
+    <a href="#" x-on:click="filtered = ! filtered"><?= lang('Bonfire.filter')?></a>
 </div>
