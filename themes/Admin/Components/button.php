11df6e6a05beda4129a75c0f2cc2be4a7d064857
Donatas Glodenis <dg@lapas.info>
2025-02-25 23:44:46 +0200

    Update button component with name and id properties

--- themes/Admin/Components/button.php
+++ themes/Admin/Components/button.php
@@ -1,3 +1,9 @@
-<button <?= isset($onclick) ? 'onclick="' . $onclick . '"' : '' ?> type="<?= $type ?? 'submit' ?>" class="btn btn-<?= $color ?? 'primary' ?> btn-lg">
+<button
+    <?= isset($onclick) ? 'onclick="' . $onclick . '"' : '' ?>
+    type="<?= $type ?? 'submit' ?>"
+    class="btn btn-<?= $color ?? 'primary' ?> btn-lg"
+    <?= isset($id) ? 'id="' . $id . '"' : '' ?>
+    <?= isset($name) ? 'name="' . $name . '"' : '' ?>
+>
     <?= $slot ?? '' ?>
 </button>
